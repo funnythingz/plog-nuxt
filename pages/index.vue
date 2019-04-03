a8e7d773b25685ad7bc856d592de12e39952aabb
funnythingz <funnythingz69+github@gmail.com>
2019-04-04 01:24:09 +0900

    changed to 2col

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,37 @@
 <template lang="pug">
 v-container
-  v-layout.row.justify-center.mb-3
-    v-flex.text-sm-right.lg6
-      v-btn
-        v-icon.mr-1(color="lime")
-          | mdi-plus-circle-outline
-        span
-          | Create
   v-layout.row.justify-center
-    v-flex.lg6
-      v-card.mb-2(v-for="comment of comments" :key="comment.id" color="deep-purple darken-1" dark)
-        v-card-text
-          | {{comment.content}}
-        v-card-actions
-          v-btn(color="red" flat)
-            v-icon.mr-1
-              | mdi-heart-broken
-            | Bad!
-            span.small.ml-2
-              | {{comment.bad}}
-          v-btn(color="green" flat)
-            v-icon.mr-1
-              | mdi-heart-circle-outline
-            | Good!
-            span.small.ml-2
-              | {{comment.good}}
+    v-flex.lg6.pr-3
+      v-layout.row
+        v-flex
+          v-card.mb-2(v-for="comment of comments" :key="comment.id" color="deep-purple darken-1" dark)
+            v-card-text
+              | {{comment.content}}
+            v-divider(dark)
+            v-card-actions.justify-end
+              v-btn(color="green" flat)
+                v-icon.mr-1
+                  | mdi-heart-circle-outline
+                | Good!
+                span.small.ml-2
+                  | {{comment.good}}
+              v-btn(color="red" flat)
+                v-icon.mr-1
+                  | mdi-heart-broken
+                | Bad!
+                span.small.ml-2
+                  | {{comment.bad}}
+    v-flex.lg2
+      v-layout.row
+        v-flex
+          v-form(@submit.prevent="submit")
+            v-textarea(v-model="comment" outline label="Comment" color="indigo lighten-1")
+            v-layout.row.justify-end
+              v-flex.text-sm-right
+                v-btn
+                  v-icon.mr-1
+                    | mdi-comment-plus
+                  | post
 </template>
 
 <script>
@@ -35,6 +42,12 @@ export default {
 
   created() {
     this.$store.dispatch('setCommentsRef', db.collection('comments'))
+  },
+
+  data() {
+    return {
+      comment: ''
+    }
   },
 
   computed: {
